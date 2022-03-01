7aa97191fa882e524c48801a91ac7bafdc390368
cutewonny <cutewonny@ad-max.co.kr>
2022-03-01 19:03:46 +0900

    docker and postgres

---
+++ src/test/java/com/inf_loop_dev/center/CenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inf_loop_dev.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
